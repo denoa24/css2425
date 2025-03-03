c84df2d9a6c1f4cb339c1163af12a4e3fd7cb0bd
denoa24 <denis.vna24@gmail.com>
2025-03-03 21:31:05 +0200

    curs6

---
+++ curs6/js/book.js
@@ -0,0 +1,18 @@
+export function Book(title,author,page){
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.toString = toString;
+}
+
+function toString(){
+    // return this.title + ' de ' + this.author + ', si are '+ this.page + ' pagini.'
+    return `${this.title} de   ${this.author}, 
+    si are ${this.page} pagini.`
+}
+
+export function citeste(){
+    return "Acum citesc o carte";
+}
+// // export {Book};
+// export {Book, citeste};
